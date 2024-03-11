4a860bf58516d5ca39375b6f075d8f714215cefd
Mahenvs <147363796+Mahenvs@users.noreply.github.com>
2024-03-11 01:06:09 -0500

    Time: 65 ms (69.00%), Space: 52.8 MB (51.55%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -3,7 +3,6 @@ function isPalindrome(s: string): boolean {
     const input: string = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
     let i = 0;
     let j = input.length - 1;
-    console.log(i,j,input.charAt(i) , input.charAt(j),input.length)
     
     while (i <= j) {
 
