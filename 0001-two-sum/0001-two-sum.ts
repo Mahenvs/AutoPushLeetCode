3b3ca4b65f6c251f14edf7e520a6fe98078d462c
Mahenvs <147363796+Mahenvs@users.noreply.github.com>
2024-03-11 00:40:36 -0500

    Time: 61 ms (76.51%), Space: 53.3 MB (13.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,11 +1,11 @@
 function twoSum(nums: number[], target: number): number[] {
     const map = new Map<number, number>();
 
-    let result: number[] = [];
+
     for (let i = 0; i < nums.length; i++) {
         const valueToFind = target - nums[i];
         if (map.has(valueToFind)) 
-            return result = [map.get(valueToFind), i]
+            return [map.get(valueToFind), i]
         else 
             map.set(nums[i], i)
         
