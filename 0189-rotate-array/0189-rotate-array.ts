af99351ae65e10ec4401d4c4153b98b23ec398c2
Mahenvs <147363796+Mahenvs@users.noreply.github.com>
2024-03-12 16:18:40 -0500

    Time: 84 ms (80.60%), Space: 59.1 MB (54.40%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.ts
+++ 0189-rotate-array/0189-rotate-array.ts
@@ -4,11 +4,11 @@
 function rotate(nums: number[], k: number): void {
     
     k =  k % nums.length ;
-    if(nums.length > 1)
-{    reverse(nums,0,nums.length-1);  
+    
+ reverse(nums,0,nums.length-1);  
     reverse(nums,0,k-1);
     reverse(nums,k,nums.length-1)
-}    
+    
 };
 
 function reverse(nums:number[], start:number,end: number){
